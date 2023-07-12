35ef50fbc4ac623abef50b1e1105666066ecc1f2
furstamp <gyket.pentest.133971@protonmail.com>
2023-07-12 02:16:31 -0300

    template de layout de set comum dos shaders

---
+++ voxygen/src/render/shader_set.rs
@@ -0,0 +1,23 @@
+// padrão
+use std::marker::PhantomData;
+
+// local
+use super::{
+    Pipeline,
+    RenderErr
+};
+
+pub struct ShaderSet<P: Pipeline> {
+    phantom: PhantomData<P>
+}
+
+impl<P: Pipeline> ShaderSet<P> {
+    pub fn new(
+        vs: &[u8],
+        fs: &[u8]
+    ) -> Result<Self, RenderErr> {
+        Ok(Self {
+            phantom: PhantomData
+        })
+    }
+}
