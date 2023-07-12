f87364ff9f2691e6b0e9c689757ce925e966d797
furstamp <gyket.pentest.133971@protonmail.com>
2023-07-12 00:59:09 -0300

    atributos físicos para diretório de comp

---
+++ common/src/comp/phys.rs
@@ -0,0 +1,30 @@
+// biblioteca
+use specs::{Component, VecStorage};
+use vek::*;
+
+// pos
+
+#[derive(Copy, Clone, Debug)]
+pub struct Pos(pub Vec3<f32>);
+
+impl Component for Pos {
+    type Storage = VecStorage<Self>;
+}
+
+// vel
+
+#[derive(Copy, Clone, Debug)]
+pub struct Vel(pub Vec3<f32>);
+
+impl Component for Vel {
+    type Storage = VecStorage<Self>;
+}
+
+// dir
+
+#[derive(Copy, Clone, Debug)]
+pub struct Dir(pub Vec3<f32>);
+
+impl Component for Dir {
+    type Storage = VecStorage<Self>;
+}
