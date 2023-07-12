0b5a365203cf2a0f7332e31ce3e364222d79cc9a
furstamp <gyket.pentest.133971@protonmail.com>
2023-07-12 19:59:05 -0300

    implementação de mais consts locais e fatores de pipelines

--- voxygen/src/render/model.rs
+++ voxygen/src/render/model.rs
@@ -1,18 +1,39 @@
-// padrão
-use std::marker::PhantomData;
+// biblioteca
+use gfx::{
+    self,
+
+    traits::FactoryExt
+};
 
 // local
-use super::Pipeline;
+use super::{
+    mesh::Mesh,
+    Pipeline,
+    gfx_backend
+};
 
 // representa o mesh que foi enviado para a cpu
 pub struct Model<P: Pipeline> {
-    phantom: PhantomData<P>
+    pub vbuf: gfx::handle::Buffer<gfx_backend::Resources, P::Vertex>,
+
+    pub slice: gfx::Slice<gfx_backend::Resources>
 }
 
 impl<P: Pipeline> Model<P> {
-    pub fn new() -> Self {
+    pub fn new(
+        factory: &mut gfx_backend::Factory,
+        mesh: &Mesh<P>
+    ) -> Self {
         Self {
-            phantom: PhantomData
+            vbuf: factory.create_vertex_buffer(mesh.vertices()),
+
+            slice: gfx::Slice {
+                start: 0,
+                end: mesh.vertices().len() as u32,
+                base_vertex: 0,
+                instances: None,
+                buffer: gfx::IndexBuffer::Auto
+            }
         }
     }
 }
