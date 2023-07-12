f063abf48026f2d96a6916de495760a243eeac0d
furstamp <gyket.pentest.133971@protonmail.com>
2023-07-12 17:26:09 -0300

    pipeline de vertex de renderização de personagem

---
+++ voxygen/src/render/pipelines/character.rs
@@ -0,0 +1,44 @@
+// biblioteca
+use gfx::{
+    self,
+    VertexBuffer,
+    ConstantBuffer,
+    RenderTarget,
+    DepthTarget,
+    preset::depth::LESS_EQUAL_WRITE,
+
+    // macros
+    gfx_defines,
+    gfx_vertex_struct_meta,
+    gfx_constant_struct_meta,
+    gfx_impl_struct_meta,
+    gfx_pipeline,
+    gfx_pipeline_inner
+};
+
+// local
+use super::super::{
+    renderer::{
+        TgtColorFmt,
+        TgtDepthFmt
+    }
+};
+
+gfx_defines! {
+    vertex Vertex {
+        pos: [f32; 3] = "v_pos",
+        bone: u8 = "v_bone"
+    }
+
+    constant Locals {
+        model: [[f32; 4]; 4] = "u_model"
+    }
+
+    pipeline pipe {
+        vbuf: VertexBuffer<Vertex> = (),
+        locals: ConstantBuffer<Locals> = "locals",
+        
+        tgt_color: RenderTarget<TgtColorFmt> = "tgt",
+        tgt_depth: DepthTarget<TgtDepthFmt> = LESS_EQUAL_WRITE
+    }
+}
