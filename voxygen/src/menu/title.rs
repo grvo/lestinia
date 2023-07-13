2cb09f435b6640014505f37e44dffeae9d7126a9
furstamp <gyket.pentest.133971@protonmail.com>
2023-07-12 23:12:30 -0300

    adição ajudantes para o arquivo que lida com título

--- voxygen/src/menu/title.rs
+++ voxygen/src/menu/title.rs
@@ -6,33 +6,48 @@ use crate::{
     PlayState,
     PlayStateResult,
     GlobalState,
-    window::Event
+    window::Event,
+    render,
+    session::SessionState
 };
 
 pub struct TitleState;
 
 impl TitleState {
+    /// cria um novo `titlestate`
+
     pub fn new() -> Self {
         Self
     }
 }
 
-const BG_COLOR: Rgba<f32> = Rgba { r: 0.0, g: 0.3, b: 1.0, a: 1.0 };
+// a cor do background
+const BG_COLOR: Rgba<f32> = Rgba { r: 0.8, g: 1.0, b: 0.8, a: 1.0 };
 
 impl PlayState for TitleState {
     fn play(&mut self, global_state: &mut GlobalState) -> PlayStateResult {
-        'eventloop: loop {
-            // processar eventos da janela
+        loop {
+            // ajuda os eventos da janela
             for event in global_state.window.fetch_events() {
                 match event {
-                    Event::Close => break 'eventloop PlayStateResult::Shutdown
+                    Event::Close => return PlayStateResult::Shutdown,
+
+                    // quando o espaço é pressionado, iniciar uma sessão
+                    Event::Char(' ') => return PlayStateResult::Push(
+                        Box::new(SessionState::from_renderer(global_state.window.renderer_mut()))
+                    ),
+
+                    // ignorar todos os outros eventos
+                    _ => {}
                 }
             }
 
+            // limpar a tela
             global_state.window
                 .renderer_mut()
                 .clear(BG_COLOR);
 
+            // finalizar o frame
             global_state.window
                 .renderer_mut()
                 .flush();
@@ -42,4 +57,6 @@ impl PlayState for TitleState {
                 .expect("falha ao mostrar janela");
         }
     }
+
+    fn name(&self) -> &'static str { "título" }
 }
