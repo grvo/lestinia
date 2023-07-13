8b07fe7d9bfa01ef30fefa907b31634a9db05121
furstamp <gyket.pentest.133971@protonmail.com>
2023-07-12 23:26:51 -0300

    adição de medidas mesh para objetos triangulares

--- voxygen/src/render/mesh.rs
+++ voxygen/src/render/mesh.rs
@@ -1,25 +1,38 @@
 // local
 use super::Pipeline;
 
-/// utilizado para armazenar dado de vertex na cpu
+/// um estrutura de mesh de tipo `vec` utilizado para armazenar dados de mesh na cpu
 pub struct Mesh<P: Pipeline> {
     verts: Vec<P::Vertex>
 }
 
 impl<P: Pipeline> Mesh<P> {
+    /// criar um novo `mesh`
     pub fn new() -> Self {
         Self { verts: vec![] }
     }
 
+    /// obter fatia referenciando os vértices desta malha
     pub fn vertices(&self) -> &[P::Vertex] {
         &self.verts
     }
 
+    /// puxar novo vertex para o final desta malha
     pub fn push(&mut self, vert: P::Vertex) {
         self.verts.push(vert);
     }
 
+    /// puxar novo polígono para o final desta malha
+    pub fn push_tri(&mut self, tri: Tri<P>) {
+        self.verts.push(tri.a);
+        self.verts.push(tri.b);
+        self.verts.push(tri.c);
+    }
+
+    /// empurrar novo quad para o final desta malha.
     pub fn push_quad(&mut self, quad: Quad<P>) {
+        // um quad é composto por dois triângulos
+
         // tri 1
         self.verts.push(quad.a.clone());
         self.verts.push(quad.b);
@@ -32,7 +45,24 @@ impl<P: Pipeline> Mesh<P> {
     }
 }
 
-/// representa um quad
+/// representa um triângulo armazenado na cpu
+pub struct Tri<P: Pipeline> {
+    a: P::Vertex,
+    b: P::Vertex,
+    c: P::Vertex,
+}
+
+impl<P: Pipeline> Tri<P> {
+    pub fn new(
+        a: P::Vertex,
+        b: P::Vertex,
+        c: P::Vertex,
+    ) -> Self {
+        Self { a, b, c }
+    }
+}
+
+/// representa um quad armazenado na cpu
 pub struct Quad<P: Pipeline> {
     a: P::Vertex,
     b: P::Vertex,
