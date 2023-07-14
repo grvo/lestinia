659fc761e29c2e830d610b8a7ec7a252125a77fd
furstamp <gyket.pentest.133971@protonmail.com>
2023-07-13 22:35:47 -0300

    adição da função `maintain_gpu_data()`

--- voxygen/src/scene/mod.rs
+++ voxygen/src/scene/mod.rs
@@ -33,7 +33,7 @@ impl Scene {
             camera: Camera::new(),
 
             globals: renderer
-                .create_consts_with(Globals::new())
+                .create_consts_with(Globals::default())
                 .unwrap(),
 
             skybox: Skybox {
@@ -42,10 +42,29 @@ impl Scene {
                     .unwrap(),
 
                 locals: renderer
-                    .create_consts_with(SkyboxLocals::new())
+                    .create_consts_with(SkyboxLocals::default())
                     .unwrap()
             }
         }
+    }
+
+    pub fn maintain_gpu_data(&mut self, renderer: &mut Renderer) {
+        // computar matrizes de câmera
+        let (view_mat, proj_mat, cam_pos) = self.camera.compute_dependents();
+
+        // atualizar constantes globais
+        renderer.update_consts(&mut self.globals, Globals::new(
+            view_mat,
+            proj_mat,
+            cam_pos,
+            
+            self.camera.get_focus_pos(),
+
+            10.0,
+            0.0,
+            0.0
+        ))
+            .expect("falha ao atualizar constantes globais");
     }
 
     /// renderizar cena usando o `renderer` fornecido
