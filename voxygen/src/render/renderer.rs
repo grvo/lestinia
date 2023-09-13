c85bc600e7defe6819470218f54746b748c72375
grivo <90540827+grvo@users.noreply.github.com>
2023-09-13 15:33:35 -0300

    atualização de diversos componentes de renderer

--- voxygen/src/render/renderer.rs
+++ voxygen/src/render/renderer.rs
@@ -23,24 +23,25 @@ use super::{
 
     pipelines::{
         Globals,
-        character,
+        figure,
         skybox
     }
 };
 
-/// representa o formato da cor da janela
-pub type TgtColorFmt = gfx::format::Srgba8;
+/// representa o formato da cor de janela
+pub type TgtColorFmt = gfx::format::Rgba8;
 
 /// representa o formato da profundidade da janela
 pub type TgtDepthFmt = gfx::format::DepthStencil;
 
-/// auxilia o alvo da cor da janela
+/// uma ajuda para o alvo da cor da janela
 pub type TgtColorView = gfx::handle::RenderTargetView<gfx_backend::Resources, TgtColorFmt>;
 
-/// auxilia o alvo da profundidade da janela
+/// uma ajuda para o alvo da profundidade da janela
 pub type TgtDepthView = gfx::handle::DepthStencilView<gfx_backend::Resources, TgtDepthFmt>;
 
-/// tipagem que encapsula estado renderizado
+/// um tipo que encapsula o estado renderizado. renderer é central à renderização de voxygen
+/// e contém qualquer estado necessário para tal
 pub struct Renderer {
     device: gfx_backend::Device,
     encoder: gfx::Encoder<gfx_backend::Resources, gfx_backend::CommandBuffer>,
@@ -50,7 +51,7 @@ pub struct Renderer {
     tgt_depth_view: TgtDepthView,
 
     skybox_pipeline: GfxPipeline<skybox::pipe::Init<'static>>,
-    character_pipeline: GfxPipeline<character::pipe::Init<'static>>
+    figure_pipeline: GfxPipeline<figure::pipe::Init<'static>>
 }
 
 impl Renderer {
@@ -62,7 +63,7 @@ impl Renderer {
         tgt_color_view: TgtColorView,
         tgt_depth_view: TgtDepthView
     ) -> Result<Self, RenderError> {
-        // constrói uma pipeline para as skyboxes renderizadas
+        // constrói uma pipeline para renderizar skyboxes
         let skybox_pipeline = create_pipeline(
             &mut factory,
             skybox::pipe::new(),
@@ -71,13 +72,13 @@ impl Renderer {
             include_bytes!(concat!(env!("CARGO_MANIFEST_DIR"), "/shaders/skybox.frag"))
         )?;
 
-        // constrói uma pipeline para os caracteres renderizados
-        let character_pipeline = Self::new_pipeline(
+        // constrói uma pipeline para renderizar skyboxes
+        let figure_pipeline = create_pipeline(
             &mut factory,
-            character::pipe::new(),
+            figure::pipe::new(),
 
-            include_bytes!(concat!(env!("CARGO_MANIFEST_DIR"), "/shaders/character.vert")),
-            include_bytes!(concat!(env!("CARGO_MANIFEST_DIR"), "/shaders/character.frag"))
+            include_bytes!(concat!(env!("CARGO_MANIFEST_DIR"), "/shaders/figure.vert")),
+            include_bytes!(concat!(env!("CARGO_MANIFEST_DIR"), "/shaders/figure.frag"))
         )?;
 
         Ok(Self {
@@ -89,65 +90,59 @@ impl Renderer {
             tgt_depth_view,
 
             skybox_pipeline,
-            character_pipeline
+            figure_pipeline
         })
     }
 
-    /// enfileira a limpeza de cor e profundidade para um novo frame a ser renderizado
-    pub fn clear(&mut self, col: Rgba<f32>) {
+    /// lista a limpeza de cor e profundidade prontos para um novo frame ser renderizado
+    /// TODO: fazer a versão disso que não limpe o alvo colorido por rapidez
+    fn clear(&mut self, col: Rgba<f32>) {
         self.encoder.clear(&self.tgt_color_view, col.into_array());
         self.encoder.clear_depth(&self.tgt_depth_view, 1.0);
     }
 
-    /// performa todas as chamadas de draw nesse frame e limpa os itens descartados
+    /// performar todas as listas de desenhos que chamam esse frame e limpa os itens descartados
     pub fn flush(&mut self) {
         self.encoder.flush(&mut self.device);
         self.device.cleanup();
     }
 
-    /// cria uma nova pipeline para o shader de vertex e fragmento de shader fornecido
-    pub fn create_consts<T: Copy + gfx::traits::Pod>(&mut self) -> Result<Consts<T>, RenderError> {
-        Ok(Consts::new(&mut self.factory))
-    }
-
-    /// cria uma nova variedade de constantes com um valor
-    pub fn create_consts_with<T: Copy + gfx::traits::Pod>(
+    /// cria um novo conjunto de constantes com os valores fornecidos
+    pub fn create_consts<T: Copy + gfx::traits::Pod> {
         &mut self,
-        val: T
-    ) -> Result<Consts<T>, RenderError> {
-        let mut consts = self.create_consts()?;
+        vals: &[T]
+    } -> Result<Consts<T>, RenderError> {
+        let mut consts = Consts::new(&mut self.factory, vals.len());
 
-        consts.update(&mut self.encoder, val)?;
+        consts.update(&mut self.encoder, vals)?;
 
         Ok(consts)
     }
 
-    /// atualiza o conjunto de constantes com novo valor
-    pub fn update_consts<T: Copy + gfx::traits::Pod>(
+    /// atualizar lista de conjuntos com os valores fornecidos
+    pub fn update_consts<T: Copy + gfx::traits::Pod> {
         &mut self,
         consts: &mut Consts<T>,
-        val: T
-    ) -> Result<(), RenderError> {
-        consts.update(&mut self.encoder, val)
+        vals: &[T]
+    } -> Result<(), RenderError> {
+        consts.update(&mut self.encoder, vals)
     }
 
-    /// criar novo modelo por meio do mesh fornecido
+    /// cria um novo modelo a partir do mesh fornecido
     pub fn create_model<P: Pipeline>(&mut self, mesh: &Mesh<P>) -> Result<Model<P>, RenderError> {
         Ok(Model::new(
             &mut self.factory,
-
             mesh
         ))
     }
 
-    /// enfileirar a renderização do modelo de skybox fornecido no frame que virá em breve
+    /// lista a renderização do modelo de skybox fornecido
     pub fn render_skybox(
         &mut self,
 
         model: &Model<skybox::SkyboxPipeline>,
-
-        locals: &Consts<skybox::Locals>,
-        globals: &Consts<Globals>
+        globals: &Consts<Globals>,
+        locals: &Consts<skybox::Locals>
     ) {
         self.encoder.draw(
             &model.slice,
@@ -155,8 +150,36 @@ impl Renderer {
 
             &skybox::pipe::Data {
                 vbuf: model.vbuf.clone(),
+
                 locals: locals.buf.clone(),
                 globals: globals.buf.clone(),
+
+                tgt_color: self.tgt_color_view.clone(),
+                tgt_depth: self.tgt_depth_view.clone()
+            }
+        );
+    }
+
+    /// lista a renderização com o modelo de figura fornecido
+    pub fn render_figure(
+        &mut self,
+
+        model: &Model<skybox::FigurePipeline>,
+        globals: &Consts<Globals>,
+        locals: &Consts<figure::Locals>,
+        bones: &Consts<figure::BoneData>
+    ) {
+        self.encoder.draw(
+            &model.slice,
+            &self.figure_pipeline.pso,
+
+            &figure::pipe::Data {
+                vbuf: model.vbuf.clone(),
+
+                locals: locals.buf.clone(),
+                globals: globals.buf.clone(),
+
+                bones: bones.buf.clone(),
 
                 tgt_color: self.tgt_color_view.clone(),
                 tgt_depth: self.tgt_depth_view.clone()
@@ -169,10 +192,9 @@ struct GfxPipeline<P: gfx::pso::PipelineInit> {
     pso: gfx::pso::PipelineState<gfx_backend::Resources, P::Meta>
 }
 
-/// cria uma nova pipeline para o shader e fragmento de vertex fornecido
+/// cria uma nova pipeline a partir do shader de vertex fornecido
 fn create_pipeline<'a, P: gfx::pso::PipelineInit>(
     factory: &mut gfx_backend::Factory,
-    
     pipe: P,
 
     vs: &[u8],
@@ -184,31 +206,29 @@ fn create_pipeline<'a, P: gfx::pso::PipelineInit>(
 
     Ok(GfxPipeline {
         pso: factory.create_pipeline_from_program(
-                &program,
+            &program,
 
-                gfx::Primitive::TriangleList,
+            gfx::Primitive::TriangleList,
 
-                gfx::state::Rasterizer {
-                    front_face: gfx::state::FrontFace::CounterClockwise,
-                    cull_face: gfx::state::CullFace::Back,
+            gfx::state::Rasterizer {
+                front_face: gfx::state::FrontFace::CounterClockwise,
+                cull_face: gfx::state::CullFace::Back,
+                method: gfx::state::RasterMethod::Fill,
 
-                    method: gfx::state::RasterMethod::Fill,
-                    offset: None,
+                offset: None,
+                samples: Some(gfx::state::MultiSample)
+            },
 
-                    samples: Some(gfx::state::MultiSample)
-                },
-
-                pipe
-            )
-                .map_err(|err| RenderError::PipelineError(match err {
-                    gfx::PipelineStateError::Program(err) =>
-                        gfx::PipelineStateError::Program(err),
-
-                    gfx::PipelineStateError::DescriptorInit(err) =>
-                        gfx::PipelineStateError::DescriptorInit(err.into()),
-
-                    gfx::PipelineStateError::DeviceCreate(err) =>
-                        gfx::PipelineStateError::DeviceCreate(err)
-                }))?
+            pipe
+        )
+            // fazer coisas ao redor de oddity
+            .map_err(|err| RenderError::PipelineError(match err {
+                gfx::PipelineStateError::Program(err) =>
+                    gfx::PipelineStateError::Program(err),
+                gfx::PipelineStateError::DescriptorInit(err) =>
+                    gfx::PipelineStateError::DescriptorInit(err.into()),
+                gfx::PipelineStateError::DeviceCreate(err) =>
+                    gfx::PipelineStateError::DeviceCreate(err),
+            }))?
     })
 }
