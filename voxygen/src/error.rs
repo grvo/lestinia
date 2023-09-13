dde5c879935f6fa38472fc32ac00e123b0e72d14
grivo <90540827+grvo@users.noreply.github.com>
2023-09-13 09:48:00 -0300

    adição de propriedade de erro para voxygen

--- voxygen/src/error.rs
+++ voxygen/src/error.rs
@@ -1,12 +1,18 @@
 // padrão
 use std::any;
 
+// projeto
+use client;
+
 // caixote
 use crate::render::RenderError;
 
 /// representa qualquer erro que podem ser avisados pelo voxygen
 #[derive(Debug)]
 pub enum Error {
+    /// erro relacionado ao client interno
+    ClientError(client::Error),
+    
     /// erro diverso relacionado a uma dependência de backend
     BackendError(Box<any::Any>),
 
@@ -20,5 +26,11 @@ pub enum Error {
 impl From<RenderError> for Error {
     fn from(err: RenderError) -> Self {
         Error::RenderError(err)
+    }
+}
+
+impl From<client::Error> for Error {
+    fn from(err: client::Error) -> Self {
+        Error::ClientError(err)
     }
 }
