14f91a82a40efa64252584bd6f5f433e4e22d802
grivo <90540827+grvo@users.noreply.github.com>
2023-09-18 09:26:57 -0300

    algumas adições para mod de terrenos

--- common/src/terrain/mod.rs
+++ common/src/terrain/mod.rs
@@ -1,32 +1,45 @@
 pub mod block;
 pub mod biome;
 
+// re-exportações
+pub use self::{
+    block::Block,
+    biome::BiomeKind
+};
+
 // biblioteca
 use vek::*;
 
 // caixote
 use crate::{
     vol::VolSize,
-    volumes::vol_map::VolMap
+    
+    volumes::{
+        vol_map::VolMap,
+        chunk::Chunk
+    }
 };
 
-// local
-use self::{
-    block::Block,
-    biome::BiomeKind
-};
+// terrainchunksize
+pub struct TerrainChunkSize;
 
-// chunksize
-pub struct ChunkSize;
-
-impl VolSize for ChunkSize {
+impl VolSize for TerrainChunkSize {
     const SIZE: Vec3<u32> = Vec3 { x: 32, y: 32, z: 32 };
 }
 
-// chunkmeta
-pub struct ChunkMeta {
+// terrainchunkmeta
+pub struct TerrainChunkMeta {
     biome: BiomeKind
 }
 
-// terrainmap
-pub type TerrainMap = VolMap<Block, ChunkSize, ChunkMeta>;
+impl TerrainChunkMeta {
+    pub fn void() -> Self {
+        Self {
+            biome: BiomeKind::Void
+        }
+    }
+}
+
+// tipo de terreno
+pub type TerrainChunk = Chunk<Block, TerrainChunkSize, TerrainChunkMeta>;
+pub type TerrainMap = VolMap<Block, TerrainChunkSize, TerrainChunkMeta>;
