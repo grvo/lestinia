2879f829ff00d80150b291c7de3eccf577f96e18
grivo <90540827+grvo@users.noreply.github.com>
2023-09-19 09:43:11 -0300

    adição de diversas propriedades de limpeza e ticks

--- client/src/lib.rs
+++ client/src/lib.rs
@@ -4,6 +4,7 @@ use std::time::Duration;
 // biblioteca
 use specs::Entity as EcsEntity;
 use vek::*;
+use threadpool;
 
 // projeto
 use common::{
@@ -25,8 +26,10 @@ pub struct Input {
 }
 
 pub struct Client {
-    state: State,
+    thread_pool: threadpool::ThreadPool,
 
+    tick: u64,
+    state: State,
     player: Option<EcsEntity>,
 
     // teste
@@ -39,8 +42,12 @@ impl Client {
     /// cria um novo `client`
     pub fn new() -> Self {
         Self {
-            state: State::new(),
+            thread_pool: threadpool::Builder::new()
+                .thread_name("lestinia-worker".into())
+                .build(),
 
+            tick: 0,
+            state: State::new(),
             player: None,
 
             // teste
@@ -49,18 +56,42 @@ impl Client {
         }
     }
 
-    /// TODO: obter rid disso
+    /// obtém a referência do threadpool do worker do client.
+    ///
+    /// esse pool deve ser utilizado para qualquer opearação expansiva que roda fora da thread principal
+    /// como por exemplo, thread que bloqueiam operações i/o
+    pub fn thread_pool(&self) -> &threadpool::ThreadPool {
+        &self.thread_pool
+    }
+
+    // todo: obtém o rid disso
     pub fn with_test_state(mut self) -> Self {
         self.chunk = Some(self.world.generate_chunk(Vec3::zero()));
 
         self
     }
 
+    /// todo: obtém o rid disso
+    pub fn load_chunk(&mut self, pos: Vec3<i32>) {
+        self.state.terrain_mut().insert(pos, self.world.generate_chunk(pos));
+
+        self.state.changes_mut().new_chunks.push(pos);
+    }
+
     /// obtém a referência para o estado do jogo do cliente
-    pub fn state(&self) -> &state { &self.state }
+    pub fn state(&self) -> &state {
+        &self.state
+    }
 
     /// obtém a referência mutável para estado do jogo do cliente
-    pub fn state_mut(&mut self) -> &mut State { &mut self.state }
+    pub fn state_mut(&mut self) -> &mut State {
+        &mut self.state
+    }
+
+    /// obtém o número de tick atual
+    pub fn get_tick(&self) -> u64 {
+        self.tick
+    }
 
     /// executar tick de cliente único, ajudar input e atualizar estado do jogo pela duração recebida
     pub fn tick(&mut self, input: Input, dt: Duration) -> Result<(), Error> {
@@ -79,6 +110,14 @@ impl Client {
         self.state.tick(dt);
 
         // finalizar o tick, passar controle de volta para o frontend (passo 6)
+        self.tick += 1;
+        
         Ok(())
+    }
+
+    /// limpar o client depois de um tick
+    pub fn cleanup(&mut self) {
+        // limpar o estado local
+        self.state.cleanup();
     }
 }
