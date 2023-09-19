3fae16085df77446a3f7a83e13e4d9013c2b6fc6
grivo <90540827+grvo@users.noreply.github.com>
2023-09-19 09:35:39 -0300

    implementação de arquivo das bibliotecas

---
+++ world/src/lib.rs
@@ -0,0 +1,31 @@
+// biblioteca
+use vek::*;
+
+// projeto
+use common::{
+    vol::Vox,
+
+    terrain::{
+        Block,
+        
+        TerrainChunk,
+        TerrainChunkMeta
+    }
+};
+
+#[derive(Debug)]
+pub enum Error {
+    Other(String)
+}
+
+pub struct World;
+
+impl World {
+    pub fn new() -> Self {
+        Self
+    }
+
+    pub fn generate_chunk(&self, pos: Vec3<i32>) -> TerrainChunk {
+        TerrainChunk::filled(Block::empty(), TerrainChunkMeta::void())
+    }
+}
