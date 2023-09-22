81ac226a579b56642d9973cf0edb47b2097c25a4
grivo <90540827+grvo@users.noreply.github.com>
2023-09-22 08:05:07 -0300

    implementação de arquivo para texturas

---
+++ voxygen/src/render/texture.rs
@@ -0,0 +1,67 @@
+// padrão
+use std::marker::PhantomData;
+
+// biblioteca
+use gfx::{
+    self,
+
+    traits::{
+        Factory,
+        FactoryExt
+    }
+};
+
+use image::{
+    DynamicImage,
+    GenericImageView
+};
+
+// local
+use super::{
+    RenderError,
+    mesh::Mesh,
+    Pipeline,
+    gfx_backend
+};
+
+type ShaderFormat = (gfx::format::R8_G8_B8_A8, gfx::format::Srgb);
+
+/// representa uma imagem que foi postada na gpu
+pub struct Texture<P: Pipeline> {
+    pub tex: gfx::handle::Texture<gfx_backend::Resources, <ShaderFormat as gfx::format::Formatted>::Surface>,
+    pub srv: gfx::handle::ShaderResourceView<gfx_backend::Resources, <ShaderFormat as gfx::format::Formatted>::View>,
+    pub sampler: gfx::handle::Sampler<gfx_backend::Resources>, _phantom: PhantomData<P>
+}
+
+impl<P: Pipeline> Texture<P> {
+    pub fn new(
+        factory: &mut gfx_backend::Factory,
+        image: &DynamicImage
+    ) -> Result<Self, RenderError> {
+        let (tex, srv) = factory.create_texture_immutable_u8::<ShaderFormat>(
+            gfx::texture::Kind::D2(
+                image.width() as u16,
+                image.height() as u16,
+
+                gfx::texture::AaMode::Single
+            ),
+
+            gfx::texture::Mipmap::Provided,
+
+            &[&image.to_rgba().into_raw()]
+        )
+            .map_err(|err| RenderError::CombinedError(err))?;
+
+        Ok(Self {
+            tex,
+            srv,
+
+            sampler: factory.create_sampler(gfx::texture::SamplerInfo::new(
+                gfx::texture::FilterMethod::Scale,
+                gfx::texture::WrapMode::Clamp
+            )),
+
+            _phantom: PhantomData
+        })
+    }
+}
