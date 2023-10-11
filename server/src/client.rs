c8036d0d90e9140c66b73be9da31f3c88db8ef2e
grivo <90540827+grvo@users.noreply.github.com>
2023-10-11 09:14:44 -0300

    estrutura de client para diretório de servidor

---
+++ server/src/client.rs
@@ -0,0 +1,16 @@
+use specs::Entity as EcsEntity;
+
+use common::{
+	msg::{
+		ServerMsg,
+		ClientMsg
+	},
+
+	net::PostBox
+};
+
+pub struct Client {
+	pub ecs_entity: EcsEntity,
+	pub postbox: PostBox<ServerMsg, ClientMsg>,
+	pub last_ping: f64
+}
