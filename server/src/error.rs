a69ba59adad69e4c96fdaf7c1e7b83be21e187bb
grivo <90540827+grvo@users.noreply.github.com>
2023-10-11 09:15:51 -0300

    mensagens e logs de erros

---
+++ server/src/error.rs
@@ -0,0 +1,14 @@
+use common::net::PostError;
+
+#[derive(Debug)]
+pub enum Error {
+	Network(PostError),
+
+	Other(String)
+}
+
+impl From<PostError> for Error {
+	fn from(err: PostError) -> Self {
+		Error::Network(err)
+	}
+}
