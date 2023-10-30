26be9945f6892d956bf3ae27c1d249121ac23ca3
grivo <90540827+grvo@users.noreply.github.com>
2023-10-30 09:31:02 -0300

    registro de componentes locais

--- common/src/comp/mod.rs
+++ common/src/comp/mod.rs
@@ -13,6 +13,5 @@ pub fn register_local_components(ecs_world: &mut EcsWorld) {
     ecs_world.register::<phys::Pos>();
     ecs_world.register::<phys::Vel>();
     ecs_world.register::<phys::Dir>();
-    
     ecs_world.register::<phys::UpdateKind>();
 }
