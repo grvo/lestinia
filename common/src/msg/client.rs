382841cbb55f65effd58d0a9733a5a6348ad77d5
grivo <90540827+grvo@users.noreply.github.com>
2023-11-01 09:26:38 -0300

    importando propriedades para client de mensagens

--- common/src/msg/client.rs
+++ common/src/msg/client.rs
@@ -1,6 +1,21 @@
+use crate::comp::{
+	Uid,
+
+	phys
+};
+
 #[derive(Clone, Debug, Serialize, Deserialize)]
 pub enum ClientMsg {
+	Ping,
+	Pong,
+	
 	Chat(String),
+
+	PlayerPhysics {
+		pos: phys::Pos,
+		vel: phys::Vel,
+		dir: phys::Dir
+	},
 
 	Disconnect
 }
