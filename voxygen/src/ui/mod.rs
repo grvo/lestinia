811535084e2c01f73af8a94fb7eeb4fd191f5475
grivo <90540827+grvo@users.noreply.github.com>
2023-11-11 00:38:22 -0300

    atualização de função com os frames

--- voxygen/src/ui/mod.rs
+++ voxygen/src/ui/mod.rs
@@ -1,16 +1,35 @@
-pub mod element;
-pub mod size_request;
-pub mod span;
+// pub mod element;
+// pub mod size_request;
+// pub mod span;
 
 // re-exportações
-pub use self::{
+/* pub use self::{
     span::Span,
 
     size_request::SizeRequest
-};
+}; */
+
+// todo: qual era o propósito do request de tamanho?
+// todo: armazenar todo o render da ui
+// todo: será preciso armazenar locals para cada widget?
+// todo: tamanhos? : renderer.get_resolution().map(|e| e as f32)
 
 // biblioteca
 use image::DynamicImage;
+
+use conrod_core::{
+	Ui as CrUi,
+	UiBuilder,
+	UiCell,
+
+	image::{
+		Map,
+
+		Id as ImgId
+	},
+
+	widget::Id as WidgId
+};
 
 // caixote
 use crate::{
@@ -22,18 +41,22 @@ use crate::{
 
         Model,
         Texture,
+		
         UiPipeline,
+		UiLocals,
+
+		Consts,
 
         create_ui_quad_mesh
     }
 };
 
 // local
-use self::element::{
+/* use self::element::{
     Element,
 
     Bounds
-};
+}; */
 
 #[derive(Debug)]
 pub enum UiError {
@@ -63,38 +86,78 @@ impl Cache {
 }
 
 pub struct Ui {
-    base: Box<dyn Element>,
-    cache: Cache
+	ui: CrUi,
+	image_map: Map<Texture<UiPipeline>>,
+    cache: Cache,
+	locals: Consts<UiLocals>
 }
 
 impl Ui {
-    pub fn new<E: Element>(renderer: &mut Renderer, base: Box<E>) -> Result<Self, Error> {
+    pub fn new(renderer: &mut Renderer, dim: [f64; 2]) -> Result<Self, Error> {
         Ok(Self {
-            base,
-
-            cache: Cache::new(renderer)?
+			ui: UiBuilder::new(dim).build(),
+			image_map: Map::new(),
+            cache: Cache::new(renderer)?,
+			locals: renderer.create_consts(&[UiLocals::default()])?
         })
     }
 
+	pub fn new_image(&mut self, renderer: &mut Renderer, image: &DynamicImage) -> Result<ImgId, Error> {
+		Ok(self.image_map.insert(renderer.create_texture(image)?))
+	}
+
+	pub fn new_widget(&mut self) -> WidgId {
+		self.ui.widget_id_generator().next()
+	}
+
+	pub fn set_widgets<F>(&mut self, f: F) where F: FnOnce(&mut UiCell) {
+		f(&mut self.ui.set_widgets());
+	}
+
+	// todo: mudar o render para &self e utilizar maintain para operações mutáveis
     pub fn maintain(&mut self, renderer: &mut Renderer) {
-        self.base.maintain(
-            renderer,
-            &self.cache,
-
-            Bounds::new(0.0, 0.0, 1.0, 1.0),
-
-            renderer.get_resolution().map(|e| e as f32)
-        )
+        // renderer.get_resolution().map(|e| e as f32)
     }
 
-    pub fn render(&self, renderer: &mut Renderer) {
-        self.base.render(
-            renderer,
-            &self.cache,
+    pub fn render(&mut self, renderer: &mut Renderer) {
+        if let Some(mut primitives) = Some(self.ui.draw()) {
+			// renderizar os primitivos de uma vez
+			while let Some(prim) = primitives.next() {
+				use conrod_core::render::{
+					Primitive,
+					PrimitiveKind
+				};
 
-            Bounds::new(0.0, 0.0, 1.0, 1.0),
-            
-            renderer.get_resolution().map(|e| e as f32)
-        );
+				let Primitive {
+					kind,
+					scizzor,
+					rect,
+
+					..
+				} = prim;
+
+				match kind {
+					PrimitiveKind::Image {
+						image_id,
+						color,
+						source_rect
+					} => {
+						renderer.update_consts(&mut self.locals, &[UiLocals::new(
+                            [0.0, 0.0, 1.0, 1.0]
+                        )]);
+
+						let tex = self.image_map.get(&image_id).expect("imagem não existe no mapa de imagens");
+
+						renderer.render_ui_element(&self.cache.model(), &self.locals, &tex);
+					}
+
+					PrimitiveKind::Other {..} => {println!("tipo primitivo outro com id");}
+                    PrimitiveKind::Rectangle {..} => {println!("tipo primitivo de rect");}
+                    PrimitiveKind::Text {..} => {println!("tipo primitivo de texto");}
+                    PrimitiveKind::TrianglesMultiColor {..} => {println!("tipo primitivo multicolorido");}
+                    PrimitiveKind::TrianglesSingleColor {..} => {println!("tipo primitivo de cor único");}
+				}
+			}
+		}
     }
 }
