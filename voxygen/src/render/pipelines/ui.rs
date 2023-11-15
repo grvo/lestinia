9aad44890cd99f471a2ec0cd754c51e74cbb5b95
grivo <90540827+grvo@users.noreply.github.com>
2023-11-15 18:14:02 -0300

    ajustes gerais de interface gráfica

--- voxygen/src/render/pipelines/ui.rs
+++ voxygen/src/render/pipelines/ui.rs
@@ -6,7 +6,6 @@ use gfx::{
     gfx_defines,
 
     gfx_vertex_struct_meta,
-    gfx_constant_struct_meta,
     gfx_impl_struct_meta,
 
     gfx_pipeline,
@@ -26,41 +25,24 @@ use super::super::{
 
 gfx_defines! {
     vertex Vertex {
-        pos: [f32; 3] = "v_pos",
+        pos: [f32; 2] = "v_pos",
         uv: [f32; 2] = "v_uv"
     }
 
     constant Locals {
-        bounds: [f32; 4] = "bounds"
+        color: [f32; 4] = "v_color",
+		mode: u32 = "v_mode"
     }
 
     pipeline pipe {
         vbuf: gfx::VertexBuffer<Vertex> = (),
 
-        locals: gfx::ConstantBuffer<Locals> = "u_locals",
         tex: gfx::TextureSampler<[f32; 4]> = "u_tex",
+
+		scissor: gfx::Scissor = (),
 
         tgt_color: gfx::BlendTarget<TgtColorFmt> = ("tgt_color", gfx::state::ColorMask::all(), gfx::preset::blend::ALPHA),
         tgt_depth: gfx::DepthTarget<TgtDepthFmt> = gfx::preset::depth::PASS_TEST
-    }
-}
-
-impl Locals {
-    pub fn default() -> Self {
-        Self {
-            bounds: [
-                0.0,
-                0.0,
-                1.0,
-                1.0
-            ]
-        }
-    }
-
-    pub fn new(bounds: [f32; 4]) -> Self {
-        Self {
-            bounds
-        }
     }
 }
 
@@ -70,31 +52,52 @@ impl Pipeline for UiPipeline {
     type Vertex = Vertex;
 }
 
-pub fn create_quad_mesh() -> Mesh<UiPipeline> {
-    let mut mesh = Mesh::new();
+/// desenhar texto pela cache de textura do texto `tex` no shader de fragmento
+pub const MODE_TEXT: u32 = 0;
 
-    #[rustfmt::skip]
-    mesh.push_quad(Quad::new(
-        Vertex {
-            pos: [0.0, 0.0, 0.0],
-            uv: [0.0, 0.0]
-        },
+/// desenhar uma imagem pela textura em `tex` no shader de fragmento
+pub const MODE_IMAGE: u32 = 1;
 
-        Vertex {
-            pos: [0.0, 1.0, 0.0],
-            uv: [0.0, 1.0]
-        },
+/// ignorar `tex` e desenhar simple, geometria 2d colorida
+pub const MODE_GEOMETRY: u32 = 2;
 
-        Vertex {
-            pos: [1.0, 1.0, 0.0],
-            uv: [1.0, 1.0]
-        },
+pub enum Mode {
+	Text,
+	Image,
+	Geometry
+}
 
-        Vertex {
-            pos: [1.0, 0.0, 0.0],
-            uv: [1.0, 0.0]
-        }
-    ));
+impl Mode {
+	fn value(self) -> u32 {
+		match Self {
+			Mode::Text => MODE_TEXT,
+			Mode::Image => MODE_IMAGE,
+			Mode::Geometry => MODE_GEOMETRY
+		}
+	}
+}
 
-    mesh
+// todo: não utilizar [f32; 4] para retângulos como formatos
+pub fn push_quad_to_mesh(mesh: &mut Mesh<UiPipeline>, rect: [f32; 4], uv_rect: [f32; 4], color: [f32; 4], mode: Mode) {
+	let mode_val = mode.value();
+
+	let v = |pos, uv| {
+		Vertex {
+			pos,
+			uv,
+			color,
+
+			mode: mode_val
+		}
+	};
+
+	let (l, t, r, b) = (rect[0], rect[1], rect[2], rect[3]);
+	let (uv_l, uv_t, uv_r, uv_b) = (uv_rect[0], uv_rect[1], uv_rect[2], uv_rect[3]);
+
+	mesh.push_quad(Quad::new(
+		v([r, t], [uv_r, uv_t]),
+        v([l, t], [uv_l, uv_t]),
+        v([l, b], [uv_l, uv_b]),
+        v([r, b], [uv_r, uv_b])
+	));
 }
